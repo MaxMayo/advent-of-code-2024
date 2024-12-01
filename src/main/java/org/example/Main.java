9bc8a4894ae0dd600dea56a3c6dbf962162b6830
jordon <jordonboswell@gmail.com>
2024-12-01 12:18:53 +0000

    Day 1, no prep

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+        Day01 day01 = new Day01();
+        int result = day01.sum2();
+        System.out.println(result);
+
+    }
+}
