f28e0b93e643f78ff32e24861c19381b5c84a60c
jordon <jordonboswell@gmail.com>
2024-12-09 22:59:35 +0000

    tower and map loading 👍

--- src/main/java/org/example/Day08.java
+++ src/main/java/org/example/Day08.java
@@ -2,15 +2,46 @@ package org.example;
 
 import org.example.utils.AbstractDay;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day08 extends AbstractDay {
     public Day08(List<String> lines) {
         super(lines);
     }
 
+    private boolean isTower(char character) {
+        return Character.isAlphabetic(character) || Character.isDigit(character);
+    }
+
     @Override
     public long partOne() {
+        // load map into grid
+        int width = lines.getFirst().length();
+        int height = lines.size();
+        char[][] originalMap = new char[height][width];
+        for(int i = 0; i < height; i++) {
+            originalMap[i] = lines.get(i).toCharArray();
+        }
+
+        // note all tower locations and group them sensibly
+        Map<Character, List<Point>> towerLocations = new HashMap<>();
+        for(int row = 0; row < height; row++) {
+            for(int column = 0; column < width; column++) {
+                char character = originalMap[row][column];
+                if(isTower(character)) {
+                    if(!towerLocations.containsKey(character)) {
+                        towerLocations.put(character, new ArrayList<>());
+                    }
+                    towerLocations.get(character).add(new Point(row, column));
+                }
+            }
+        }
+
+
         return 0;
     }
 
