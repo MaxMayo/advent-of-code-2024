f54b573a2733c2e689026e8545867b471e17d2e5
jordon <jordonboswell@gmail.com>
2024-12-09 22:40:09 +0000

    day 8 prep with more unit testing for easier development than other days

---
+++ src/main/java/org/example/Day08.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.utils.AbstractDay;
+
+import java.util.List;
+
+public class Day08 extends AbstractDay {
+    public Day08(List<String> lines) {
+        super(lines);
+    }
+
+    @Override
+    public long partOne() {
+        return 0;
+    }
+
+    @Override
+    public long partTwo() {
+        return 0;
+    }
+}
