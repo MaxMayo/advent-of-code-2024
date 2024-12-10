c2086b3c6dfbca462d469875683e1bf26dca2bb0
jordon <jordonboswell@gmail.com>
2024-12-10 22:24:30 +0000

    day 11 prep

---
+++ src/main/java/org/example/Day11.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.utils.AbstractDay;
+
+import java.util.List;
+
+public class Day11 extends AbstractDay {
+    public Day11(List<String> lines) {
+        super(lines);
+    }
+
+    @Override
+    public long partOne() {
+        return 0;
+    }
+
+    @Override
+    public long partTwo() {
+        return 0;
+    }
+}
---
+++ src/test/java/org/example/days/Day11Test.java
@@ -0,0 +1,14 @@
+package org.example.days;
+
+import org.example.AbstractTest;
+
+import java.io.IOException;
+
+public class Day11Test extends AbstractTest {
+    public Day11Test() throws IOException, ClassNotFoundException {
+        super(0);
+//        setPartOneRealResult();
+//        setExpectedExampleTwoResult();
+//        setPartTwoRealResult();
+    }
+}
