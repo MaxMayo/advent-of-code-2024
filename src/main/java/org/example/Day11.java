9c7afd1d04cbcf40c925bd1b35b1a83011975785
Jordon Boswell <jordon.boswell@dvla.gov.uk>
2024-12-11 10:43:28 +0000

    very nice part 1

--- src/main/java/org/example/Day11.java
+++ src/main/java/org/example/Day11.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import org.example.utils.AbstractDay;
 
 import java.util.List;
@@ -11,7 +12,32 @@ public class Day11 extends AbstractDay {
 
     @Override
     public long partOne() {
-        return 0;
+        long sumStones = 0;
+        int targetDepth = 25;
+        String[] startingStones = lines.getFirst().split(" ");
+        for (int i = 0; i < startingStones.length; i++) {
+            sumStones += numStones(startingStones[i], targetDepth);
+        }
+        return sumStones;
+    }
+
+    // recursion 🎺🎺
+    public long numStones(String stone, int targetDepth) {
+        if (targetDepth == 0) return 1L;
+        long sumStones = 0;
+        List<String> newStones = new ArrayList<>();
+        if(stone.equals("0")) {
+            newStones.add("1");
+        } else if (stone.length() % 2 == 0) {
+            newStones.add(String.valueOf(Long.parseLong(stone.substring(0, stone.length() / 2))));
+            newStones.add(String.valueOf(Long.parseLong(stone.substring(stone.length() / 2))));
+        } else {
+            newStones.add(String.valueOf(Long.parseLong(stone) * 2024));
+        }
+        for (String newStone : newStones) {
+            sumStones += numStones(newStone, targetDepth - 1);
+        }
+        return sumStones;
     }
 
     @Override
--- src/test/java/org/example/days/Day11Test.java
+++ src/test/java/org/example/days/Day11Test.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 
 public class Day11Test extends AbstractTest {
     public Day11Test() throws IOException, ClassNotFoundException {
-        super(0);
-//        setPartOneRealResult();
+        super(55312L);
+        setPartOneRealResult(183484L);
 //        setExpectedExampleTwoResult();
 //        setPartTwoRealResult();
     }
